bddf163eafd89cf1f8b9d5b24e41f5a13a389055
Masaaki Yana <nativeyana@gmail.com>
2020-02-19 14:24:30 -0800

    items-countrollers.js: Feature: createItem and updateItem for POST and PUT

--- backand/controllers/items-controllers.js
+++ backand/controllers/items-controllers.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid/v4'); // Create a unique id
+
 const HttpError = require('../models/error-http');
 
 DUMMY_ITEMS = [
@@ -75,9 +77,72 @@ const getItemById = (req, res, next) => {
 
 const createItem = (req, res, next) => {
     console.log('POST item in item-controller')
-    res.json({message: 'Test: reached createItem'})
+    const { title, 
+        content, 
+        image, 
+        coordinates, 
+        address, 
+        postal_code, 
+        user_id } = req.body;
+    const source = "trashapp"
+    let newItem = {
+        outcome: null,
+        user_id: 000000,
+        post_id: uuid(),
+        footer: null,
+        url: "https://trashnothing.com/public-post/29948116/true-detective-season-1-dvd-eastlake-oakland",
+        title: title,
+        longitude: -122.2711137,
+        content: content,
+        photos: image,
+        source: source,
+        date: "2020-02-18T17:46:01",
+        latitude: 37.8043637,
+        group_id: null,
+        type: "offer"
+    }
+
+    DUMMY_ITEMS.push(newItem);
+    res.status(201).json({ item: newItem })
 }
 
+const updateItem = (req, res, next) => {
+    console.log('UPDATE item in item-controller')
+    const itemId = req.params.itemId;
+
+    const indexItem = DUMMY_ITEMS.findIndex(i => i.post_id === itemId);
+    console.log(indexItem);
+    if(indexItem < 0) {
+        throw new HttpError('Item not found.', 404);
+    }
+
+    const updateItem = { ...DUMMY_ITEMS[indexItem] };
+
+    const { title, 
+        content, 
+        image, 
+        coordinates, 
+        address, 
+        postal_code, 
+        user_id } = req.body;
+
+    updateItem.title = title || updateItem.title;
+    updateItem.content = content || updateItem.content ;
+    updateItem.photos = image || updateItem.photos;
+    updateItem.postal_code = postal_code || updateItem.postal_code;
+    updateItem.address = address || updateItem.address;
+    updateItem.coordinates = coordinates || updateItem.coordinates;
+
+    DUMMY_ITEMS[indexItem] = updateItem;
+    res.status(200).json({item: updateItem})
+}
+
+const deleteItem = (req, res, next) => {
+    console.log('DELETE item in item-controller')
+    res.json({message: 'Test: reached deleteItem'})
+}
 exports.getAllItems = getAllItems;
 exports.getItemById = getItemById;
 exports.createItem = createItem;
+exports.updateItem = updateItem ;
+exports.deleteItem = deleteItem ;
--- backand/routes/items-routes.js
+++ backand/routes/items-routes.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get('/', itemsController.getAllItems); 
 router.get('/:itemId', itemsController.getItemById);
 router.post('/', itemsController.createItem); 
+router.put('/:itemId', itemsController.updateItem); 
+router.delete('/:itemId', itemsController.deleteItem); 
 
 module.exports = router;
