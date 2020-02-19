65a9d063c2a56923671d960f19eb3ec77fd5e4ac
Joseph Reid <joseph.james.reid@gmail.com>
2020-02-18 19:32:52 -0800

    Empty Components

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+class Card extends Component {
+  render() {
+
+    return (
+      <div className="Card-Title">
+      </div>
+    );
+  }
+}
+
+export default Card;
