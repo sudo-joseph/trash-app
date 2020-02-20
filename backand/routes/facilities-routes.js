145897572ac539ff6abca8bf8fb441199d9bad95
Masaaki Yana <nativeyana@gmail.com>
2020-02-20 15:19:08 -0800

    facilities-routes.js: Improvement: imports individual functions

--- backand/routes/facilities-routes.js
+++ backand/routes/facilities-routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
-const facilitiesController = require('../controllers/facilities-controllers')
+const { getAllFacilities, 
+        getFacilityById,
+        getMaterialsFromE911,
+        getFacilitiesFromE911 } = require('../controllers/facilities-controllers');
 
 const router = express.Router();
 
-router.get('/', facilitiesController.getAllFacilities); 
-router.get('/earth911/materials', facilitiesController.getMaterialsFromE911);
-router.get('/earth911/facilities', facilitiesController.getFacilitiesFromE911);
-router.get('/:facId', facilitiesController.getFacilityById);
+router.get('/', getAllFacilities); 
+router.get('/earth911/materials', getMaterialsFromE911);
+router.get('/earth911/facilities', getFacilitiesFromE911);
+router.get('/:facId', getFacilityById);
 
 module.exports = router;
