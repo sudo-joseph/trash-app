65da462d0ab9377cb240cabf9d6224f77c7db1fc
Masaaki Yana <nativeyana@gmail.com>
2020-02-20 09:37:29 -0800

    fetch-trashnothing.js: Bug: typo and proper coord/radius parameters

--- backand/utils/fetch-trashnothing.js
+++ backand/utils/fetch-trashnothing.js
@@ -4,15 +4,15 @@ const dotenv = require('dotenv');
 const HttpError = require('../models/error-http'); 
 
 dotenv.config();
-const API_KEY_TN = process.env.API_KEY_TRASHNOTHING;
+const API_KEY = process.env.API_KEY_TRASHNOTHING;
 
 
-const getDonationItems = async (cordinates, radius) => {
-    // cordinates and radius are required for querying Items(Posts) 
-    const queryRadius = cordinates || 1;
-    const queryCordinates = radius || { lat: 37.804829, lng: -122.272476 };
+const getDonationItems = async (coordinates, radius) => {
+    // coordinates and radius are required for querying Items(Posts) 
+    const queryRadius =  radius || 140;
+    const queryCordinates = coordinates || { lat: 37.804829, lng: -122.272476 };
 
-    let url = `https://trashnothing.com/api/v1.1/posts?radius=${queryRadius}&latitude=${queryCordinates.lat}&longitude=${queryCordinates.lng}&sources=trashnothing&types=offer&api_key=${API_KEY_TN}`
+    let url = `https://trashnothing.com/api/v1.1/posts?radius=${queryRadius}&latitude=${queryCordinates.lat}&longitude=${queryCordinates.lng}&sources=trashnothing&types=offer&api_key=${API_KEY}`
 
     const res = await axios.get(url);
     const data = res.data;
