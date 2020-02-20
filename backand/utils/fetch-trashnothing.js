fdabaf2d8dff791d27f78579e0211ff3e962bd10
Masaaki Yana <nativeyana@gmail.com>
2020-02-20 09:13:07 -0800

    fetch-trashnothing.js: Feature: Accept radius and coordinates as parameter, Bug: 422 when no item returned

--- backand/utils/fetch-trashnothing.js
+++ backand/utils/fetch-trashnothing.js
@@ -7,13 +7,17 @@ dotenv.config();
 const API_KEY_TN = process.env.API_KEY_TRASHNOTHING;
 
 
-const getDonationItems = async () => {
-    let url = `https://trashnothing.com/api/v1.1/posts?radius=140&latitude=37.804829&longitude=-122.272476&sources=trashnothing&types=offer&api_key=${API_KEY_TN}`
+const getDonationItems = async (cordinates, radius) => {
+    // cordinates and radius are required for querying Items(Posts) 
+    const queryRadius = cordinates || 1;
+    const queryCordinates = radius || { lat: 37.804829, lng: -122.272476 };
+
+    let url = `https://trashnothing.com/api/v1.1/posts?radius=${queryRadius}&latitude=${queryCordinates.lat}&longitude=${queryCordinates.lng}&sources=trashnothing&types=offer&api_key=${API_KEY_TN}`
 
     const res = await axios.get(url);
     const data = res.data;
 
-    if (!data) {
+    if (!data || data.posts.length === 0) {
         throw new HttpError('Could not find any post items from trashnothing.', 422);
     }
     const items = data.posts;
