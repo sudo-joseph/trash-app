2244b7d8039d4eebf954dfe63f5931f74db30267
Victor Sanchez <incomingvrs@gmail.com>
2020-02-19 21:50:13 -0800

    LandingPage.js: Put back to original

--- client/src/components/pages/LandingPage/LandingPage.js
+++ client/src/components/pages/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ class LandingPage extends Component {
         <header className="LandingPage-header">
           <p>
             Kickstart Coding - Frontend React<br />
-            NoTrash Starter Project
+            MERN Starter Project
           </p>
           <Link to="/blog/">Blog</Link>
           <Link to="/write/">Write article</Link>
