ece973966525fd9ac761ae004a9053c360049344
Masaaki Yana <nativeyana@gmail.com>
2020-02-20 13:44:34 -0800

    facilities-routes.js: Feature: GET /api/facilities/{materials,facilities} routes

--- backand/routes/facilities-routes.js
+++ backand/routes/facilities-routes.js
@@ -5,6 +5,8 @@ const facilitiesController = require('../controllers/facilities-controllers')
 const router = express.Router();
 
 router.get('/', facilitiesController.getAllFacilities); 
+router.get('/earth911/materials', facilitiesController.getMaterialsFromE911);
+router.get('/earth911/facilities', facilitiesController.getFacilitiesFromE911);
 router.get('/:facId', facilitiesController.getFacilityById);
 
 module.exports = router;
