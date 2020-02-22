6df63757a82d4949765b8ce0d6f1f4477c7235c9
Joseph Reid <joseph.james.reid@gmail.com>
2020-02-21 22:38:08 -0800

    Update Navbar Component. Add react-css-burger to dependencies & readme.

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import Burger from 'react-css-burger';
+
 import './NavBar.css';
 
 class NavBar extends Component {
+  state={active:false}
+
+
   render() {
 
     return (
-      <div className="NavBar-Title">
-      </div>
+      <div className="NavBar">
+        <div className="NavBar-Burger">
+          <Burger
+            onClick={() => this.setState({ active: !this.state.active })}
+            active={this.state.active}
+            burger="spin"
+            color="yellow"
+            hoverOpacity={0.8}
+            scale={1.2}
+          />
+        </div>
+        <div className="NavBar-Title">
+          <h1 className="App-title">Trash App</h1>
+        </div>
+     </div>
     );
   }
 }
