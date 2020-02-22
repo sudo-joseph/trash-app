9dfe2fa1e9104ed8a8c78e151fce9389b830ff36
MasaY <ishinan@users.noreply.github.com>
2020-02-21 17:42:12 -0800

    Revert "BackEnd: First PR for initial API backend server ( no storing data in MongoDB)"

--- backand/models/error-http.js
+++
@@ -1,8 +0,0 @@
-class HttpError extends Error {
-    constructor(message, httpStatusCode) {
-        super(message); // message goes to Error class
-        this.code = httpStatusCode; // non 2xxx code
-    }
-}
-
-module.exports = HttpError;
