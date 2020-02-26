5f131de3eb08da24e28b2187797ba7a5e8cbd143
Masaaki Yana <nativeyana@gmail.com>
2020-02-25 16:20:55 -0800

    Modal: Functionality of Modal was tested and functional

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,31 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 
-improt './Modal.css';
+import Backdrop from '../Backdrop/Backdrop';
+import './Modal.css';
 
 // Modal Internal Component(Not Shared)
-const ModayOverlay = props => {
+const ModalOverlay = props => {
   const content = (
     <div className={`modal ${props.className}`} style={props.style}>
-      <header className={`modal_header ${props.headerClass}`}>
+      <header className={`modal__header ${props.headerClass}`}>
         <h2>{props.header}</h2>
       </header>
       {/* Felexible form style block holds "div(body)" and "footer" */}
       <form onSubmit={props.onSubmit ? props.onSubmit : (e) => e.preventDefault}>
-        <div className={`modal_content ${props.contentClass}`}>
+        <div className={`modal__content ${props.contentClass}`}>
           {props.children}
         </div>
-        <footer className={`modal_footer ${props.footerClass}`}>
+        <footer className={`modal__footer ${props.footerClass}`}>
           {props.footer}
         </footer>
       </form>
     </div>
   );
-  return ReactDOM.createPortal(content, document.getElementById('modal-hok'));
+  return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
 }
 
 const Modal = props => {
-    
+  return (
+   <React.Fragment>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+          in={props.show}
+          mountOnEnter
+          unmountOnExit
+          timeout={200}
+          classNames="modal" >
+          <ModalOverlay {...props} />
+      </CSSTransition>
+  </React.Fragment>    
+  );
 };
 
 export default Modal;
