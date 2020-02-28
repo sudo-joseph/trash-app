9ff44376333d0376410802b2bab2f235a48c315e
Victor Sanchez <incomingvrs@gmail.com>
2020-02-28 11:31:27 -0800

    modified general App.css background image to fill for all pages despite content

--- client/src/components/pages/Materials/Materials.js
+++ client/src/components/pages/Materials/Materials.js
@@ -16,9 +16,7 @@ class Materials extends Component {
             <h1>Materials</h1>
             <h4>The categories listed in your search cover many types of materials and objects:</h4>
             {this.props.itemCategories}
-            <p>Camembert de normandie jarlsberg camembert de normandie. Melted cheese cheese slices chalk and cdheese mozzarella stilton port-salut feta danish fontina. Parmesan danish fontina fromage frais cheesy feet port-salut halloumi hard cheese stinking bishop. Fromage goat.</p>
-            <p>Cheese on toast croque monsieur airedale. Cream cheese cow cow everyone loves who moved my cheese halloumi pecorino rubber cheese. Paneer cottage cheese cheese slices boursin bocconcini bocconcini monterey jack blue castello. Bavarian bergkase caerphilly mascarpone gouda parmesan fondue airedale parmesan. Bavarian bergkase port-salut mozzarella ricotta fromage frais roquefort who moved my cheese bocconcini. Bavarian bergkase rubber cheese squirty cheese manchego fromage frais babybel cheese strings say cheese. Rubber cheese gouda cheese and biscuits cheese on toast pepper jack squirty cheese brie cheddar. Cheesy feet gouda fromage frais goat mozzarella st. agur blue cheese cheese on toast paneer. Babybel.</p>
-            <p>Cheese on toast croque monsieur airedale. Cream cheese cow cow everyone loves who moved my cheese halloumi pecorino rubber cheese. Paneer cottage cheese cheese slices boursin bocconcini bocconcini monterey jack blue castello. Bavarian bergkase caerphilly mascarpone gouda parmesan fondue airedale parmesan. Bavarian bergkase port-salut mozzarella ricotta fromage frais roquefort who moved my cheese bocconcini. Bavarian bergkase rubber cheese squirty cheese manchego fromage frais babybel cheese strings say cheese. Rubber cheese gouda cheese and biscuits cheese on toast pepper jack squirty cheese brie cheddar. Cheesy feet gouda fromage frais goat mozzarella st. agur blue cheese cheese on toast paneer. Babybel.</p>
+            
         </article>
         </div>
       </div>
